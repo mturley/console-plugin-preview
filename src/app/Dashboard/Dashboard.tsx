8aea5e84c184d6c118b013425f6d880dd638bc86
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-04-11 16:40:13 -0400

    Upgrade WDS, configure static public path so MSW works in dev mode

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
 
-const Dashboard: React.FunctionComponent = () => (
-  <PageSection>
-    <Title headingLevel="h1" size="lg">Dashboard Page Title!</Title>
-  </PageSection>
-)
+const Dashboard: React.FunctionComponent = () => {
+  const [fooData, setFooData] = React.useState<Record<string, unknown>>({});
+
+  React.useEffect(() => {
+    fetch('/api/foo')
+      .then((res) => res.json())
+      .then((data) => setFooData(data));
+  }, []);
+
+  return (
+    <PageSection>
+      <Title headingLevel="h1" size="lg">
+        Dashboard Page Title!
+      </Title>
+      Mock service worker test response:
+      <pre>{JSON.stringify(fooData, null, 2)}</pre>
+    </PageSection>
+  );
+};
 
 export { Dashboard };
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('/foo', (req, res, ctx) => {
+  rest.get('/api/foo', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json({ foo: 'bar' }));
   }),
 ];
