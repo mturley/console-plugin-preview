1356ced0b7c8d067e79907b503bd921b9ef80242
Mike Turley <mike.turley@alum.cs.umass.edu>
2022-04-27 13:07:47 -0400

    Attempting to get plugin store working, running into dependency linking issues

--- stub-app/src/index.tsx
+++ stub-app/src/index.tsx
@@ -3,7 +3,29 @@ import ReactDOM from 'react-dom';
 import App from '@app/index';
 
 import { worker } from './mocks/browser';
+import { PluginStore } from 'console/frontend/packages/console-plugin-sdk/src/store';
+import { loadPluginFromURL } from 'console/frontend/packages/console-dynamic-plugin-sdk/src/runtime/plugin-loader';
+
 worker.start();
+
+const pluginStore = new PluginStore();
+
+const loadAndEnableMockPlugin = async () => {
+  try {
+    const pluginID = await loadPluginFromURL('http://localhost:9001/');
+    pluginStore.setDynamicPluginEnabled(pluginID, true);
+    console.log({
+      allowedDynamicPluginNames: pluginStore.getAllowedDynamicPluginNames(),
+      extensionsInUse: pluginStore.getExtensionsInUse(),
+      stateForTestPurposes: pluginStore.getStateForTestPurposes(),
+      dynamicPluginInfo: pluginStore.getDynamicPluginInfo(),
+    });
+  } catch (e) {
+    console.error('Error while loading plugin', e);
+  }
+};
+
+loadAndEnableMockPlugin();
 
 if (process.env.NODE_ENV !== 'production') {
   const config = {
